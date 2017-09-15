07bab97dfa09827048cbae415407088d829a3555
KorJuk69 <korjuk1@gmail.com>
2017-09-15 17:56:21 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,19 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String a = scanner.next();
 
+        boolean IsDigit = a.matches("[-+]?\\d+");
+
+        if(IsDigit){
+
+            System.out.print("yes");
+
+        }
+        else {
+
+            System.out.print("no");
+
+        }
     }
 }
