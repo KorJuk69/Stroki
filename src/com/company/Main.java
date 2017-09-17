3aa9521320ef54f20294acf8a1d1d76c4c7c3261
KorJuk69 <korjuk1@gmail.com>
2017-09-17 23:00:06 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String str = scanner.nextLine();
+
+        int dlinna_slova = 0;
+        int slovo=0;
+
+        for (int i=0;i<str.length();i++){
+
+            char simvol = str.charAt(i);
+            dlinna_slova++;
+            if (simvol == ' ') {
+
+                dlinna_slova--;
+                if(dlinna_slova>slovo) slovo = dlinna_slova;
+
+                dlinna_slova=0;
+
+            }
+
+        }
+
+        System.out.print(slovo);
+
+        //Решить проблему с проверкой последнего слова и дописать вывод самого большого слова
 
     }
 }
