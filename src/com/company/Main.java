b66cd8d19b811bd63cb6ca27bb830f9936bbe401
KorJuk69 <korjuk1@gmail.com>
2017-09-18 00:56:04 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,15 +11,24 @@ public class Main {
 
         int dlinna_slova = 0;
         int slovo=0;
+        String max_slovo = "";
 
         for (int i=0;i<str.length();i++){
 
             char simvol = str.charAt(i);
             dlinna_slova++;
+
+            max_slovo += simvol;
             if (simvol == ' ') {
 
                 dlinna_slova--;
-                if(dlinna_slova>slovo) slovo = dlinna_slova;
+                if(dlinna_slova>slovo){
+                    slovo = dlinna_slova;
+                    /*max_slovo = "";
+                    i = i - dlinna_slova;
+                    как-то так
+                     */
+                }
 
                 dlinna_slova=0;
 
@@ -28,6 +37,7 @@ public class Main {
         }
 
         System.out.print(slovo);
+        System.out.print(max_slovo);
 
         //Решить проблему с проверкой последнего слова и дописать вывод самого большого слова
 
