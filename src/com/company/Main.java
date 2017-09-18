c6fc7a5eb93cc9992c0acfa213335f5494561590
KorJuk69 <korjuk1@gmail.com>
2017-09-18 15:53:35 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,42 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String str = scanner.nextLine();
+        String napravlenie = "";
+        String rasstoyanie = "";
+        int x=0;
+        int y=0;
+
+        for (int i=0;i<str.length();i++) {
+
+            char simvol = str.charAt(i);
+            int ascii = (int)simvol;
+            if (ascii==59){
+
+                int rast = Integer.parseInt(rasstoyanie);
+                switch (napravlenie){
+                    case "North": y += rast;
+                        break;
+                    case "South": y -= rast;
+                        break;
+                    case "East": x += rast;
+                        break;
+                    case "West": x -= rast;
+                        break;
+                }
+                rasstoyanie = "";
+                napravlenie = "";
+
+            }
+            if (ascii>64) {
+                napravlenie += simvol;
+            }
+            else {
+                if ((ascii!=59)&&(ascii!=32)) rasstoyanie += simvol;
+            }
+        }
+
+        System.out.print(x + " " + y);
 
     }
 }
