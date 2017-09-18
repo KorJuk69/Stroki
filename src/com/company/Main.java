b011c5364552afc440ef2861d821d6bfb2e4d20e
KorJuk69 <korjuk1@gmail.com>
2017-09-18 15:13:36 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,44 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String podstr = scanner.nextLine();
+        String str = scanner.nextLine();
+        int yes = 0;
+        int ravnost=0;
 
+        for (int n=0;n<str.length();n++) {
+
+            char simvol_str = str.charAt(n);
+
+            for (int m=ravnost;m<podstr.length();m++) {
+
+                char simvol_podstr = podstr.charAt(m);
+
+                if (podstr.charAt(podstr.length()-1) == simvol_str){
+
+                    System.out.print("Yes");
+                    yes = 1;
+                    break;
+
+                }
+                if (simvol_podstr != simvol_str) {
+
+                    ravnost = 0;
+                    break;
+
+                }
+                else {
+                    ravnost++;
+                    break;
+                }
+            }
+
+            if (yes==1) break;
+
+            if (str.length()-n<podstr.length()){
+                System.out.print("NO");
+                break;
+            }
+        }
     }
 }
