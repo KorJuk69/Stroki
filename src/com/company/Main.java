46638101efcdd42e19ceff6c97b3c312343f6678
KorJuk69 <korjuk1@gmail.com>
2017-09-18 14:09:40 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String str = scanner.nextLine();
+        int yes = 0;
+        char bukva = ' ';
+
+        for (int n=0;n<str.length();n++) {
+
+            char simvol1 = str.charAt(n);
+
+            for (int m=1;m<str.length();m++){
+
+                char simvol2 = str.charAt(m);
+                if (simvol1==simvol2){
+                    bukva = simvol1;
+                    yes = 1;
+                    break;
+                }
+            }
+
+            if (yes==1) break;
+
+        }
+
+        System.out.print(bukva);
 
     }
 }
