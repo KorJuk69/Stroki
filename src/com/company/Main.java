d05a98509d37cb09fb5439a987c1a8a9f28c444a
KorJuk69 <korjuk1@gmail.com>
2017-09-19 13:56:43 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,19 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String str = scanner.nextLine();
 
+        for (int n=0;n<str.length();n++) {
+
+            System.out.print(str.charAt(n));
+
+            if (str.charAt(n) == ' '){
+                if (n == str.length()-1) break;
+                while (str.charAt(n+1) == ' ') {
+                    n++;
+                    if (n == str.length()) break;
+                }
+            }
+        }
     }
 }
