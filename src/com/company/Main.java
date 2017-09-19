28262fcaaf4b8866e211df0a378e5c777208af21
KorJuk69 <korjuk1@gmail.com>
2017-09-19 12:13:29 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String str = scanner.nextLine();
+        int k = scanner.nextInt();
 
+        for (int n=0;n<str.length();n++) {
+
+            char simvol = str.charAt(n);
+            byte ascii = (byte) simvol;
+            ascii -= k;
+            if (ascii<65) ascii += 26;
+            simvol = (char) ascii;
+            System.out.print(simvol);
+        }
     }
 }
