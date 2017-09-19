d3dbaedded52ffd8baf3221a24115b17e42b9d41
KorJuk69 <korjuk1@gmail.com>
2017-09-19 13:13:09 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String str = scanner.nextLine();
+        int i = scanner.nextInt();
+        int j = scanner.nextInt();
+        i--;
+        j--;
 
+        for (int n=0;n<i;n++) System.out.print(str.charAt(n));
+
+        for (int n=j-1;n>=i-1;n--) System.out.print(str.charAt(n+1));
+
+        for (int n=j+1;n<str.length();n++) System.out.print(str.charAt(n));
     }
 }
