530aad87b1d99cc9ce9a4f06cff004602f898857
KorJuk69 <korjuk1@gmail.com>
2017-09-19 17:20:37 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,21 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        Pattern pattern = Pattern.compile("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
 
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.matches())
+            System.out.print(1);
+        else
+            System.out.print(0);
     }
 }
