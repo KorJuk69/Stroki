89f531a111b5bdb9c046788b7ffddb93de68f2de
KorJuk69 <korjuk1@gmail.com>
2017-09-20 14:09:51 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        String str = scanner.nextLine();
+        int b = str.length()-1;
 
+
+        for (int n=0;n<str.length();n++) {
+
+            char simvol_plus = str.charAt(n);
+
+            for (int m = b;m<str.length() + n;m--) {
+
+                char simvol_minus = str.charAt(m);
+
+                if (simvol_minus == simvol_plus) {
+                    b = m-1;
+                    break;
+                }
+                else {
+                    /*if () {  Хз как его делать
+                        b = m-1;
+                    }
+                    else b = str.length()-1;*/
+                }
+            }
+        }
     }
 }
